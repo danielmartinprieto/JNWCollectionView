566fcdff59a987cc8ecbb949e26d06cd373ed1d1
Daniel Martín <danimartinprieto@gmail.com>
2015-12-16 18:59:20 +0100

    Added a Cocoa framework for Carthage support.

---
+++ DMPCollectionView/DMPCollectionView.h
@@ -0,0 +1,25 @@
+//
+//  DMPCollectionView.h
+//  DMPCollectionView
+//
+//  Created by Daniel Martín Prieto on 16/12/15.
+//  Copyright © 2015 AppJon. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for DMPCollectionView.
+FOUNDATION_EXPORT double DMPCollectionViewVersionNumber;
+
+//! Project version string for DMPCollectionView.
+FOUNDATION_EXPORT const unsigned char DMPCollectionViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DMPCollectionView/PublicHeader.h>
+
+#import "JNWCollectionViewFramework.h"
+#import "JNWCollectionViewCell.h"
+#import "JNWCollectionViewReusableView.h"
+#import "JNWCollectionViewLayout.h"
+#import "JNWCollectionViewListLayout.h"
+#import "JNWCollectionViewGridLayout.h"
+#import "NSIndexPath+JNWAdditions.h"
